fd5a3f072721bfc8d049657e400c1fd4777a75ec
Matthew Chon <matthewhchon@gmail.com>
2023-09-18 11:42:59 -0400

    created submission and earning form

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/new_transaction")
+def load_transaction_page():
+    return render_template("submission.html")
+
+@app.route("/new_earnings")
+def load_earnings_page():
+    return render_template("earnings.html")
