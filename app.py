7e5cd0be13ef13ee6e0803930c1674831b93e581
Matthew Chon <matthewhchon@gmail.com>
2023-09-18 19:06:56 -0400

    enabled page to retrieve data

--- app.py
+++ app.py
@@ -2,14 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def retrieve_earnings():
+    return 0
+
+def retrieve_spendings():
+    return 0
+
+def compute_utilization_percentage(earnings, spendings):
+    earnings = max(1, earnings)
+    return float('%.3f'%((spendings/earnings) * 100))
+
+def base_render(html_page, **kwargs):
+    header_data = {'earnings': retrieve_earnings(), 'spendings': retrieve_spendings(), 'utilization_threshold': 30
+    }
+    header_data['delta'] = header_data['earnings'] - header_data['spendings']
+    header_data['utilization'] = compute_utilization_percentage(header_data['earnings'], header_data['spendings'])
+    return render_template(html_page, header_data=header_data, **kwargs)
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return base_render('index.html')
 
 @app.route("/new_transaction")
 def load_transaction_page():
-    return render_template("submission.html")
+    return base_render("submission.html")
 
 @app.route("/new_earnings")
 def load_earnings_page():
-    return render_template("earnings.html")
+    return base_render("earnings.html")
